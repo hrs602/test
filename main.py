459a30fe898e7b25e473b047c4864c7a33b69101
hrs602 <hirose0321@gmail.com>
2017-06-13 22:02:41 +0900

    new future

--- main.py
+++ main.py
@@ -1,65 +1,80 @@
 #!/usr/bin/env python
 # coding: utf-8
 from gae_http_client import RequestsHttpClient
-
 from google.appengine.api import taskqueue
-
 from flask import Flask, request, abort
-
 from linebot import (
-    LineBotApi, WebhookHandler
+    LineBotApi, WebhookHandler, WebhookParser
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    InvalidSignatureError, LineBotApiError
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import requests
 import config
+import doco.client
+import httplib
+import urllib
+import logging
 
 app = Flask(__name__)
 
+
+# Line Messaging API
 line_bot_api = LineBotApi(config.CHANNEL_ACCESS_TOKEN, http_client=RequestsHttpClient)
 handler = WebhookHandler(config.CHANNEL_SECRET)
+parser = WebhookParser(config.CHANNEL_SECRET)
 
+# Docomo API
+ENDPOINT_URI = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?API_KEY='
+DOCOMO_API_KEY = '62353054416e576e4851436c6244733366316b44726f6d66522f7271492e577353717368316d4d634e5537'
+
+# Vision API
+image_url = ''
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    # 'Content-Type': 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key': 'Your key',
+}
+params = urllib.urlencode({
+    # Request parameters
+    'visualFeatures': 'Description',
+    # 'visualFeatures': 'Categories,Tags, Description, Faces, ImageType, Color, Adult',
+    # 'details': 'Celebrities'
+})
+
+logging.getLogger().setLevel(logging.DEBUG)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    #body = request.get_data(as_text=True)
+    body = request.get_data()
+    logging.debug("Request body: " + body)
 
-    # Task Queue Add
-    taskqueue.add(url='/worker',
-                  params={'body': body,
-                          'signature': signature},
-                  method="POST")
-
-    return 'OK'
-
-@app.route("/worker", methods=['POST'])
-def worker():
-    body = request.form.get('body')
-    signature = request.form.get('signature')
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
+    user = {'t': 20}  # 20:kansai character
+    docomo_client = doco.client.Client(apikey=DOCOMO_API_KEY, user=user)
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='僕と契約して、魔法少女になってよ！'))
+    events = parser.parse(body, signature)
+    for event in events:
+        if event.message.type == 'text':
+            docomo_res = docomo_client.send(utt=event.message.text,apiname='Dialogue')
+            line_bot_api.reply_message(
+                event.reply_token,
+               TextSendMessage(text=docomo_res['utt'])
+            )
+        elif event.message.type = 'image':
+            line_bot_api.reply_message(
+                event.reply_token,
+               TextSendMessage(text='image')
+            )
+    return ''
 
 
 if __name__ == "__main__":
